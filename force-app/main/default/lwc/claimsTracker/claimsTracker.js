30d51bbbb878e19619eb5ad13333fc3c015c1d05
Cody Johns <cjohns365@gmail.com>
2025-02-26 16:22:55 -0700

    Add tests for component. Add content for closed cases.

--- force-app/main/default/lwc/claimsTracker/claimsTracker.js
+++ force-app/main/default/lwc/claimsTracker/claimsTracker.js
@@ -7,6 +7,7 @@ import CLAIM_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
 import CLAIM_DESC_FIELD from '@salesforce/schema/Case.Description';
 import CLAIM_OPENED_TIME_FIELD from '@salesforce/schema/Case.CreatedDate';
 import CLAIM_CLOSED_TIME_FIELD from '@salesforce/schema/Case.ClosedDate';
+import CLAIM_IS_CLOSED_FIELD from '@salesforce/schema/Case.IsClosed';
 
 /**
  * Don't just import everything, because then users can see what they shouldn't
@@ -18,7 +19,8 @@ const CLAIM_FIELDS = [
     CLAIM_SUBJECT_FIELD,
     CLAIM_DESC_FIELD,
     CLAIM_OPENED_TIME_FIELD,
-    CLAIM_CLOSED_TIME_FIELD
+    CLAIM_CLOSED_TIME_FIELD,
+    CLAIM_IS_CLOSED_FIELD
 ];
 
 export default class ClaimsTracker extends LightningElement {
@@ -35,11 +37,14 @@ export default class ClaimsTracker extends LightningElement {
     wiredAccount(result) {
         if (result.data) {
             this.claimObj = result.data;
-            console.log(this.claimObj);
             this.loading = false;
         } else if (result.error){
             this.errorMessage = result.error.body.message;
         }
+    }
+
+    get closedMessage() {
+        return 'Claim closed at ' + this.claimObj.fields.ClosedDate.displayValue;
     }
 
     claimSteps = [

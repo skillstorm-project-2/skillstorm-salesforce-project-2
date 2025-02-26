be4c55c74c38b3acee1a7e3aab53a8d50d84e58b
pgf96 <pgfxx1@gmail.com>
2025-02-26 15:00:15 -0500

    refactor: Claim components architecture changed to dashboard handling calls and claim dashboard being display only

--- force-app/main/default/lwc/claimsTable/claimsTable.js
+++ force-app/main/default/lwc/claimsTable/claimsTable.js
@@ -1,11 +1,7 @@
-import getClaims from '@salesforce/apex/ClaimsController.getClaims';
-import { LightningElement, wire } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
+import { api, LightningElement } from 'lwc';
 
 export default class ClaimsTable extends LightningElement {
-
-    claims = []
-    error;
+    @api claims;
 
     tableRowData = [];
     tableColData = [
@@ -17,32 +13,6 @@ export default class ClaimsTable extends LightningElement {
         {label: 'Status', fieldName: 'Status', type: 'text', hideDefaultActions: 'true', resizable: 'false' },
         {label: 'Date Opened', fieldName: 'Created', type: 'date', hideDefaultActions: 'true', resizable: 'false' }
     ];
-
-
-    fieldNames = ['Id', 'Claim_Type__c', 'Claim_Status__c', 'CreatedDate'];
-
-    @wire(getClaims, {fieldNames: '$fieldNames'})
-    wiredClaims(result) {
-        if (result.data) {
-            this.claims = result.data.map(claim => ({
-                Id:  claim.Id,
-                IdLink: `/claims/claim-detail?recordId=${claim.Id}`,
-                Type: claim.Claim_Type__c,
-                Status: claim.Claim_Status__c,
-                Created: claim.CreatedDate,
-            }))
-            
-        } else {
-            this.error = result.error;
-            this.claims = [];
-            console.error(result.error);
-        }
-    }
-
-    refreshTable() {
-        refreshApex(this.wiredClaims);
-    }
-    
 }
 
 

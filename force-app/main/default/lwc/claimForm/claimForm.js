abc6e19b46e14cee7d261d0ebd3544abf8f93195
Patrick Feliciano <96318160+pgf96@users.noreply.github.com>
2025-02-28 13:40:06 -0500

    Revert "[DevOps Center] Merge WI-000004 to Testing"

--- force-app/main/default/lwc/claimForm/claimForm.js
+++ force-app/main/default/lwc/claimForm/claimForm.js
@@ -1,76 +1,34 @@
 import { LightningElement, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getPersonAccountId from '@salesforce/apex/UserUtility.getPersonAccountId';
-import ACCOUNT_ID_FIELD from '@salesforce/schema/Case.AccountId';
-import CLAIM_TYPE_FIELD from '@salesforce/schema/Case.Claim_Type__c';
-import CLAIM_STATUS_FIELD from '@salesforce/schema/Case.Claim_Status__c';
-import DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
-import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
-import STATUS_FIELD from '@salesforce/schema/Case.Status';
-import ORIGIN_FIELD from '@salesforce/schema/Case.Origin';
 
 
 export default class ClaimForm extends LightningElement {
-    personAccountId;
-    claimId;
-    currentStep = '1';
-    uploadedFilesList = [];
 
-    // fields for template
-    accountIdField = ACCOUNT_ID_FIELD;
-    claimTypeField = CLAIM_TYPE_FIELD;
-    claimStatusField = CLAIM_STATUS_FIELD;
-    descriptionField = DESCRIPTION_FIELD;
-    subjectField = SUBJECT_FIELD;
-    statusField = STATUS_FIELD;
-    originField = ORIGIN_FIELD;
+    userAccountId;
 
-
-
-    get isFirstStep() {
-        return this.currentStep === '1';
-    }
-
-    get isSecondStep() {
-        return this.currentStep === '2';
-    }
-
-    get isThirdStep() {
-        return this.currentStep === '3';
-    }
-
-    get isClaimCreated() {
-        return this.claimId;
-    }
-
-    get attachedFileNames() {
-        return this.uploadedFilesList.map(file => file.name).join(', ');
-    }
-
-    @wire(getPersonAccountId)
-    wiredAccountId({ data, error}) {
-        if (data) {
-            this.personAccountId = data;
-        } else if (error) {
-            console.error('Failed to fetch the account for the current user.')
-        }
-    }
+    // @wire(getRecord, { recordId: USER_ID, fields: [ACCOUNT_ID] })
+    // wiredUser({ error, data }) {
+    //     if (data) {
+    //         console.log(data);
+    //         this.userAccountId = data.fields.AccountId.value;
+    //     } else if (error) {
+    //         console.error('Error fetching user Account ID:', error);
+    //     }
+    // }
 
     handleSubmit(event) {
         event.preventDefault();
         const fields = event.detail.fields;
 
-        fields.claimStatusField = 'Under Review';
-        fields.statusField = 'New';
-        fields.originField = 'Web';
+        fields.Claim_Status__c = 'Under Review';
+        fields.Status = 'New';
+        fields.Origin = 'Web';
 
         this.template.querySelector('lightning-record-edit-form').submit(fields);
 
     }
 
-    // Handler for successful form submission
     handleSuccess(event) {
-        this.claimId = event.detail.id;
         this.dispatchEvent(
             new ShowToastEvent({
                 title: 'Success',
@@ -78,29 +36,6 @@ export default class ClaimForm extends LightningElement {
                 variant: 'success',
             })
         );
-
-        // increment step
-        this.incrementStep();
-    }
-
-    handleUpload(event) {
-        // Get the list of uploaded files
-        const uploadedFiles = event.detail.files;
-        uploadedFiles.forEach(file => {
-            this.uploadedFilesList = [...this.uploadedFilesList, {name: file.name, documentId: file.documentId}];
-        });
-    }
-
-    handleNext() {
-        this.incrementStep();
-    }
-
-    incrementStep() {
-        let currStep = parseInt(this.currentStep);
-        if (currStep < 3) {
-            currStep += 1;
-            this.currentStep = currStep.toString();
-        }
     }
 
 }

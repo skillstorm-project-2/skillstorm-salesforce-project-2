dd8e44640a2379f12fdcf4b58d60f05f232becc1
Cody Johns <cjohns365@gmail.com>
2025-03-03 16:32:31 -0700

    Add lwc to show only active claims for any other page

---
+++ force-app/main/default/lwc/claimsTableHomePageWrapper/claimsTableHomePageWrapper.js
@@ -0,0 +1,39 @@
+import { LightningElement, wire } from 'lwc';
+import getPersonAccountId from '@salesforce/apex/UserUtility.getPersonAccountId';
+import getClaims from '@salesforce/apex/ClaimsController.getClaims';
+
+export default class ClaimsTableHomePageWrapper extends LightningElement {
+
+    error;
+    claims;
+    personAccountId;
+    showingClaimType = 'ACTIVE';
+
+    @wire(getPersonAccountId)
+    wiredAccountId({ data, error }) {
+        if (data) {
+            this.personAccountId = data;
+        } else if (error) {
+            console.error('err: ' + error);
+            this.error = error;
+        }
+    }
+
+    @wire(getClaims, {accountId: '$personAccountId'})
+    wiredClaims(result) {
+        if (result.data) {
+            this.claims = result.data.map(claim => ({
+                Id:  claim.Id,
+                IdLink: `/claims/claim-detail?recordId=${claim.Id}`,
+                Type: claim.Claim_Type__c,
+                Status: claim.Claim_Status__c,
+                Created: claim.CreatedDate,
+            }))
+            
+        } else {
+            this.error = result.error;
+            this.claims = [];
+            console.error(result.error);
+        }
+    }
+}

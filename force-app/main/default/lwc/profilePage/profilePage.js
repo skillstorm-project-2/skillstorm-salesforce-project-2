394d3eec287df8f472af3ab3b5483c643a0260a5
Patrick Feliciano <pgfxx1@gmail.com>
2025-03-04 05:31:35 +0000

    Temporarily remove SSN because of object field error.

--- force-app/main/default/lwc/profilePage/profilePage.js
+++ force-app/main/default/lwc/profilePage/profilePage.js
@@ -3,7 +3,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import EMAIL_FIELD from '@salesforce/schema/Account.PersonEmail';
 import PHONE_FIELD from '@salesforce/schema/Account.Phone';
-import SSN_FIELD from '@salesforce/schema/Account.SSN__c';
+// import SSN_FIELD from '@salesforce/schema/Account.SSN__pc';
 import MILITARY_BRANCH_FIELD from '@salesforce/schema/Account.Military_Branch__c';
 import SERVICE_DATE_START_FIELD from '@salesforce/schema/Account.Service_Date_Start__c';
 import SERVICE_DATE_END_FIELD from '@salesforce/schema/Account.Service_Date_End__c';
@@ -16,7 +16,7 @@ export default class ProfilePage extends LightningElement {
     name = NAME_FIELD;
     email = EMAIL_FIELD;
     phone = PHONE_FIELD;
-    ssn = SSN_FIELD;
+    // ssn = SSN_FIELD;
     militaryBranch = MILITARY_BRANCH_FIELD;
     serviceDateStart = SERVICE_DATE_START_FIELD;
     serviceDateEnd = SERVICE_DATE_END_FIELD;

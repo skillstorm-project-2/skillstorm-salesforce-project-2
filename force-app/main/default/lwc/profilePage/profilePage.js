7fdae2e6f666649ccefd67e0d5d213b00ecab5ce
Patrick Feliciano <pgfxx1@gmail.com>
2025-03-04 05:25:11 +0000

    Added styling and display messages on success/errror

--- force-app/main/default/lwc/profilePage/profilePage.js
+++ force-app/main/default/lwc/profilePage/profilePage.js
@@ -1,13 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import EMAIL_FIELD from '@salesforce/schema/Account.PersonEmail';
 import PHONE_FIELD from '@salesforce/schema/Account.Phone';
-import SSN_FIELD from '@salesforce/schema/Account.SSN__pc';
+import SSN_FIELD from '@salesforce/schema/Account.SSN__c';
 import MILITARY_BRANCH_FIELD from '@salesforce/schema/Account.Military_Branch__c';
 import SERVICE_DATE_START_FIELD from '@salesforce/schema/Account.Service_Date_Start__c';
 import SERVICE_DATE_END_FIELD from '@salesforce/schema/Account.Service_Date_End__c';
 import DISCHARGE_STATUS_FIELD from '@salesforce/schema/Account.Discharge_Status__c';
 import DISABILITY_RATING_FIELD from '@salesforce/schema/Account.Disability_Rating__c';
+import getPersonAccountId from '@salesforce/apex/UserUtility.getPersonAccountId';
 
 export default class ProfilePage extends LightningElement {
 
@@ -21,17 +23,39 @@ export default class ProfilePage extends LightningElement {
     dischargeStatus = DISCHARGE_STATUS_FIELD;
     disabilityRating = DISABILITY_RATING_FIELD;
 
-    handleClick() {
-        console.log(
-            this.name,
-            this.email,
-            this.phone,
-            this.ssn,
-            this.militaryBranch,
-            this.serviceDateStart,
-            this.serviceDateEnd,
-            this.dischargeStatus,
-            this.disabilityRating)
+    personAccountId;
+
+    @wire(getPersonAccountId)
+    wiredAccountId({ data, error }) {
+        if (data) {
+            this.personAccountId = data;
+        } else if (error) {
+            console.error('err: ' + error);
+            this.error = error;
+        }
+    }
+
+    handleSuccess(event) {
+
+        // Handle successful save
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: 'Profile updated successfully',
+            variant: 'success'
+        });
+        this.dispatchEvent(toastEvent);
+    }
+
+
+    handleError(event) {
+
+        // Handle error
+        const toastEvent = new ShowToastEvent({
+            title: 'Error',
+            message: 'Error updating profile',
+            variant: 'error'
+        });
+        this.dispatchEvent(toastEvent);
     }
 
 }

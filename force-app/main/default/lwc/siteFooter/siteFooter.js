557a943823cd3aaab4e84bbfedb8cc59cc273ac1
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-03-05 19:16:56 -0600

    finished changes

--- force-app/main/default/lwc/siteFooter/siteFooter.js
+++ force-app/main/default/lwc/siteFooter/siteFooter.js
@@ -1,59 +1,59 @@
-import { LightningElement } from 'lwc';
-import vaFooterLogo from '@salesforce/resourceUrl/VAClaimsPortalFooterImage';
+import { LightningElement } from "lwc";
+import vaFooterLogo from "@salesforce/resourceUrl/VAClaimsPortalFooterImage";
 
 export default class SiteFooter extends LightningElement {
-    footerImageUrl = vaFooterLogo;
-    originalContentHeight;
+  footerImageUrl = vaFooterLogo;
+  originalContentHeight;
 
-    connectedCallback() {
-        window.addEventListener('resize', () => {
-            this.adjustFooter();
-        });
+  // connectedCallback() {
+  //     window.addEventListener('resize', () => {
+  //         this.adjustFooter();
+  //     });
 
-        window.addEventListener('popstate', () => {
-            this.handlePageChange();
-        });
-    }
+  //     window.addEventListener('popstate', () => {
+  //         this.handlePageChange();
+  //     });
+  // }
 
-    renderedCallback() {
-        this.handlePageChange();
-    }
+  // renderedCallback() {
+  //     this.handlePageChange();
+  // }
 
-    disconnectedCallback() {
-        window.removeEventListener('resize', this.adjustFooter);
-        window.removeEventListener('popstate', this.adjustFooter);
-    }
+  // disconnectedCallback() {
+  //     window.removeEventListener('resize', this.adjustFooter);
+  //     window.removeEventListener('popstate', this.adjustFooter);
+  // }
 
-    handlePageChange = () => {
-        setTimeout(() => this.adjustFooter(), 100); // Delays execution to allow content to load
-    };
+  // handlePageChange = () => {
+  //     setTimeout(() => this.adjustFooter(), 100); // Delays execution to allow content to load
+  // };
 
-    adjustFooter() {
-        const bodyHeight = document.getElementsByClassName('body')[0].offsetHeight;// the height of the content
-        
-        if (bodyHeight == 0) {
-            this.handlePageChange();
-            return;
-        }
-        
-        const viewportHeight = window.innerHeight; // the height of the user's screen
+  // adjustFooter() {
+  //     const bodyHeight = document.getElementsByClassName('body')[0].offsetHeight;// the height of the content
 
-        const footer = document.getElementsByClassName('footer')[0];
-        const navbar = document.getElementsByClassName('a11y-banner')[0];
+  //     if (bodyHeight == 0) {
+  //         this.handlePageChange();
+  //         return;
+  //     }
 
-        console.log("Adjusting footer");
+  //     const viewportHeight = window.innerHeight; // the height of the user's screen
 
-        //console.log("Body height", bodyHeight);
-        //console.log("Viewport height", viewportHeight);
+  //     const footer = document.getElementsByClassName('footer')[0];
+  //     const navbar = document.getElementsByClassName('a11y-banner')[0];
 
-        if (bodyHeight <= viewportHeight) {
-            //console.log("Footer height", footer.offsetHeight);
-            //console.log("Navbar height", navbar.offsetHeight);
+  //     console.log("Adjusting footer");
 
-            footer.style.marginTop = `${((viewportHeight + navbar.offsetHeight) - (footer.offsetHeight + bodyHeight))}px`;
-            footer.style.width = '100%';
-        } else {
-            footer.style.position = 'relative';
-        }
-    };
+  //     //console.log("Body height", bodyHeight);
+  //     //console.log("Viewport height", viewportHeight);
+
+  //     if (bodyHeight <= viewportHeight) {
+  //         //console.log("Footer height", footer.offsetHeight);
+  //         //console.log("Navbar height", navbar.offsetHeight);
+
+  //         footer.style.marginTop = `${((viewportHeight + navbar.offsetHeight) - (footer.offsetHeight + bodyHeight))}px`;
+  //         footer.style.width = '100%';
+  //     } else {
+  //         footer.style.position = 'relative';
+  //     }
+  // };
 }
